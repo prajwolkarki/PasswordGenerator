7266ae1ace090c62ee93e668a0c8131427a7fae8
Prajwol <prajwolkarki609@gmail.com>
2024-04-05 16:36:20 +0545

    Last Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const length = 16;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "1234567890";
+const symbol = "@#$%^&*()_+{}[]></\=";
+const allChars = upperCase + lowerCase + number + symbol;
+
+const password = () => {
+
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+
+}
+const copyPassword = () =>{
+    passwordBox.select();
+    document.execCommand("copy");
+}
